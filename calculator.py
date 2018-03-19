1fba61ae54825536a56ed6fd85ac8fa1572c44ef
cedar9311 <117208869@qq.com>
2018-03-19 11:05:47 +0800

    calculator.py

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import sys
+
+if __name__=='__main__':
+    a = float(sys.argv[1])-3500.00
+    if a<=1500:
+        ss=a*0.03
+    elif a<=4500:
+        ss=a*0.1-105
+    elif a<=9000:
+        ss=a*0.2-555
+    elif a<=35000:
+        ss=a*0.25-1005
+    elif a<=55000:
+        ss=a*0.3-2755
+    elif a<=80000:
+        ss=a*0.35-5505
+    elif a>80000:
+        ss=a*0.45-13505
+    else:
+        print("Parameter Error")
+    print('%.2f'%ss)
