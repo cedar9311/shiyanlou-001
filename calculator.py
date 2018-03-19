c0f560366ab054421b2b0337eb783eb6940ba468
cedar9311 <117208869@qq.com>
2018-03-19 13:21:19 +0800

    20180319

--- calculator.py
+++ calculator.py
@@ -3,8 +3,11 @@
 import sys
 
 if __name__=='__main__':
-    a = float(sys.argv[1])-3500.00
-    if a<=1500:
+    a = (float(sys.argv[1]))*(1-(0.08+0.02+0.005+0.06))-3500.00
+    b = (float(sys.argv[1]))*(1-(0.08+0.02+0.005+0.06))
+    if a<=0:
+        ss=0
+    elif a<=1500:
         ss=a*0.03
     elif a<=4500:
         ss=a*0.1-105
@@ -20,4 +23,5 @@ if __name__=='__main__':
         ss=a*0.45-13505
     else:
         print("Parameter Error")
-    print('%.2f'%ss)
+    gongzi= b-ss
+    print('%.2f'%gongzi)
